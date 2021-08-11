082e9441bf077ff7deb8d28a0dbe4b49c73cb512
T4-HADES <s3870730@rmit.edu.vn>
2021-08-11 18:33:27 +0700

    11-8

---
+++ src/StopWatch.java
@@ -0,0 +1,37 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class StopWatch {
+    private DateTimeFormatter out = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+    LocalDateTime now = LocalDateTime.now();
+
+
+    private String startTime, endTime;
+    private long s, e;
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public StopWatch() {
+        startTime = out.format(now);
+    }
+
+    public void start() {
+        startTime = out.format(now);
+        s = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        endTime = out.format(now);
+        e = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return e - s;
+    }
+}
